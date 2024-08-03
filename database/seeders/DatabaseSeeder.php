b9630f6f5d6ea55879605bf46ba0c0cffabc4169
Arizidane <samionohari@gmail.com>
2024-08-04 03:58:18 +0700

    update: database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,27 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UsersTableSeeder::class,
+            CategoriesTableSeeder::class,
+            ProductsTableSeeder::class,
+            OrdersTableSeeder::class,
+            ReviewsTableSeeder::class,
+            CartsTableSeeder::class,
+            CartItemsTableSeeder::class,
+            PaymentsTableSeeder::class,
+            ShippingsTableSeeder::class,
         ]);
     }
 }
