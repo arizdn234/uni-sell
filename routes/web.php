2b3c0485771229f638506db932e8b7f988eb1e72
Arizidane <samionohari@gmail.com>
2024-08-05 15:17:59 +0700

    feat:  breeze

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderItemController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\CartItemController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\ShippingController;
 
-// Home Route (can be customized)
 Route::get('/', function () {
     return view('welcome');
 });
 
-// User Routes
-Route::resource('users', UserController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Category Routes
-Route::resource('categories', CategoryController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Product Routes
-Route::resource('products', ProductController::class);
-
-// Order Routes
-Route::resource('orders', OrderController::class);
-
-// OrderItem Routes
-Route::resource('order-items', OrderItemController::class);
-
-// Review Routes
-Route::resource('reviews', ReviewController::class);
-
-// Cart Routes
-Route::resource('carts', CartController::class);
-
-// CartItem Routes
-Route::resource('cart-items', CartItemController::class);
-
-// Payment Routes
-Route::resource('payments', PaymentController::class);
-
-// Shipping Routes
-Route::resource('shippings', ShippingController::class);
+require __DIR__.'/auth.php';
