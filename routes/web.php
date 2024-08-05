f6ba5ac62fede804c85a012078a0ae1258d95688
Arizidane <samionohari@gmail.com>
2024-08-05 22:22:07 +0700

    fix: admin middleware routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\ProductController;
+use App\Http\Middleware\CheckAdmin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/admin-dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard')->middleware(CheckAdmin::class);
+// Route::get('/admin-dashboard/products', [ProductController::class, 'index'])->name('admin.product')->middleware(CheckAdmin::class);
+
+// Grouping admin routes with middleware
+Route::middleware(['auth', 'verified', CheckAdmin::class])->group(function () {
+    Route::get('/admin-dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+    
+    // Additional admin routes
+    Route::prefix('admin-dashboard')->group(function () {
+        Route::get('/products', [ProductController::class, 'index'])->name('admin.product');
+        // You can add more routes here for CRUD operations or other admin functionalities
+    });
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
