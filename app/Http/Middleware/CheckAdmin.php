5861f787b215e657cc3e5ca219083f8d2dc19661
Arizidane <samionohari@gmail.com>
2024-08-05 22:23:16 +0700

    feat: is_admin middleware

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (auth()->check() && auth()->user()->is_admin) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized');
+    }
+}
