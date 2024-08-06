a27dcc6fecdddb32c282123614d8b52f33ad3cf5
Arizidane <samionohari@gmail.com>
2024-08-06 16:23:11 +0700

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,15 @@ Route::middleware(['auth', 'verified', CheckAdmin::class])->group(function () {
 
         Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
         
+        // Product routes
         Route::get('/products', [ProductController::class, 'index'])->name('admin.products.index');
+        Route::get('/products/create', [ProductController::class, 'create'])->name('admin.products.create');
+        Route::post('/products', [ProductController::class, 'store'])->name('admin.products.store');
         Route::get('/products/{product}', [ProductController::class, 'show'])->name('admin.products.show');
         Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('admin.products.edit');
         Route::put('/products/{product}', [ProductController::class, 'update'])->name('admin.products.update');
         Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('admin.products.destroy');
+        // Route::get('/products/export', [ProductController::class, 'export'])->name('admin.products.export');
     });
 });
 
