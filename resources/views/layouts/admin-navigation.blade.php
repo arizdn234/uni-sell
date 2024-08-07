9a6f51e31a4b91522487335dd55619fd266d1915
Arizidane <samionohari@gmail.com>
2024-08-07 19:08:42 +0700

    fix: admin navigation

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -7,13 +7,59 @@
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
+
+                
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('admin.products.index')" :active="request()->routeIs('admin.product')">
+                    {{-- <x-nav-link :href="route('admin.products.index')" :active="request()->routeIs('admin.product')">
                         {{ __('Products') }}
-                    </x-nav-link>
+                    </x-nav-link> --}}
+
+                    <!-- Manage Dropdown -->
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                    <div>Manage</div>
+        
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+        
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('products.index')" :active="request()->routeIs('products.*')">
+                                    {{ __('Products') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('categories.index')" :active="request()->routeIs('categories.*')">
+                                    {{ __('Categories') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('orders.index')" :active="request()->routeIs('orders.*')">
+                                    {{ __('Orders') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('cart-items.index')" :active="request()->routeIs('cart-items.*')">
+                                    {{ __('Cart Items') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('payments.index')" :active="request()->routeIs('payments.*')">
+                                    {{ __('Payments') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('reviews.index')" :active="request()->routeIs('reviews.*')">
+                                    {{ __('Reviews') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('shippings.index')" :active="request()->routeIs('shippings.*')">
+                                    {{ __('Shipping') }}
+                                </x-dropdown-link>
+                                <x-dropdown-link :href="route('users.index')" :active="request()->routeIs('users.*')">
+                                    {{ __('Users') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 </div>
             </div>
             <div class="hidden sm:flex sm:items-center sm:ml-6">
