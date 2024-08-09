1cf958d444273401c31d7b890ee4f7c28c8b8fa3
Arizidane <samionohari@gmail.com>
2024-08-09 18:11:10 +0700

    fix: shipping seeder

--- database/seeders/ShippingsTableSeeder.php
+++ database/seeders/ShippingsTableSeeder.php
@@ -6,9 +6,23 @@ use Illuminate\Database\Seeder;
 use App\Models\Shipping;
 use App\Models\Order;
 use Faker\Factory as Faker;
+use Illuminate\Support\Str;
 
 class ShippingsTableSeeder extends Seeder
 {
+    private function generateUniqueTrackingNumber()
+    {
+        $uuid = Str::uuid()->toString();
+
+        $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+        $randomString = '';
+        for ($i = 0; $i < 6; $i++) {
+            $randomString .= $chars[rand(0, strlen($chars) - 1)];
+        }
+
+        return 'TRK-' . now()->timestamp . '-' . $randomString;
+    }
+
     public function run()
     {
         $faker = Faker::create();
@@ -18,8 +32,8 @@ class ShippingsTableSeeder extends Seeder
             Shipping::create([
                 'order_id' => $order->id,
                 'shipping_method' => $faker->randomElement(['standard', 'express']),
-                'tracking_number' => $faker->regexify('[A-Z0-9]{10}'),
-                'status' => 'shipped',
+                'tracking_number' => $this->generateUniqueTrackingNumber(),
+                'status' => $faker->randomElement(['shipped', 'arrived', 'processed']),
             ]);
         }
     }
