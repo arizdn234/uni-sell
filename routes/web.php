35462ebd728208dbfa4458ed62ce1015ef2de5d0
Arizidane <samionohari@gmail.com>
2024-08-10 15:48:13 +0700

    fix: client routes

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,14 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ShippingController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ClientController::class, 'dashboard'])->name('dashboard');
 
 Route::middleware(['auth', 'verified', CheckAdmin::class])->group(function () {
     Route::prefix('admin')->group(function () {
 
         Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-        
-        // Route::get('/products', [ProductController::class, 'index'])->name('admin.products.index');
-        // Route::get('/products/create', [ProductController::class, 'create'])->name('admin.products.create');
-        // Route::post('/products', [ProductController::class, 'store'])->name('admin.products.store');
-        // Route::get('/products/{product}', [ProductController::class, 'show'])->name('admin.products.show');
-        // Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('admin.products.edit');
-        // Route::put('/products/{product}', [ProductController::class, 'update'])->name('admin.products.update');
-        // Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('admin.products.destroy');
-        // Route::get('/products/export', [ProductController::class, 'export'])->name('products.export');
         
         Route::resource('products', ProductController::class);
         Route::resource('categories', CategoryController::class);
@@ -39,13 +29,23 @@ Route::middleware(['auth', 'verified', CheckAdmin::class])->group(function () {
         Route::resource('reviews', ReviewController::class);
         Route::resource('shippings', ShippingController::class);
         Route::resource('users', UserController::class);
+
     });
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [ClientController::class, 'dashboard'])->name('user.dashboard');
+    
+    // dummy routes
+    Route::get('/cart', function () {
+        return view('user.cart');
+    })->name('user.cart');
+    Route::get('/promo', function () {
+        return view('user.promo');
+    })->name('promo.page');
+    Route::get('/new', function () {
+        return view('user.new');
+    })->name('new.arrivals');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
